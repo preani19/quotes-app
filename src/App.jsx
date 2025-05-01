44a2b73b2bb0d46760d285f479600ea50f961cda
preani19 <reda.2003.alali@gmail.com>
2025-05-01 17:36:02 +0300

    ggs

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,55 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quote, setQuote] = useState('')
+  const [author, setAuthor] = useState('')
+
+
+
+  
+  function toggle() {
+    fetch('https://dummyjson.com/quotes')
+    .then(res => res.json())
+    .then(d => {
+      const randomIndex = Math.floor(Math.random() * d.quotes.length)
+      setQuote(d.quotes[randomIndex].quote)
+      setAuthor(d.quotes[randomIndex].author)
+    }); 
+  }
+  
+  
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div id="quote-box" className='max-w-128   bg-blue-300 mx-5 p-10 rounded-md'>
+      <h1 className='text-center mb-5'>Random Quote Machine</h1>
+      <div id="quote">
+        <p id="text" className='italic font-medium'>
+          {quote ? <i className="fa-solid fa-quote-left"></i>  : ""} 
+          { " " + quote + " "}
+          {quote ? <i className="fa-solid fa-quote-right"></i> : ""} 
         </p>
+        <p id="author" className='font-light mt-2'>{quote ? '- ' : ""}{author}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="flex flex-col  sm:flex-row  justify-between gap-10 sm:gap-20 mt-5">
+            {quote 
+            ? <a
+                id="tweet-quote"
+                href={`https://twitter.com/intent/tweet?text=${quote} - ${author}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className='text-center'
+                >
+                  Tweet this quote
+                </a> 
+            :   ""}
+      <button id="new-quote" onClick={toggle}>
+           {quote ? "New Quote" : "Genrate a Quete"} 
+      </button>
+      </div>
+    
+    </div>
   )
 }
 
