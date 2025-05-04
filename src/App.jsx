895e9cb1dd12b6422edbb30a3d7a339c6860aba7
preani19 <reda.2003.alali@gmail.com>
2025-05-04 17:00:23 +0300

    finshed

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
 
 
   return (
-    <div id="quote-box" className='max-w-128   bg-blue-300 mx-5 p-10 rounded-md'>
+    <div id="quote-box" className='transition hover:scale-110 duration-300 max-w-128   bg-blue-300 mx-5 p-10 rounded-md'>
       <h1 className='text-center mb-5'>Random Quote Machine</h1>
       <div id="quote">
         <p id="text" className='italic font-medium'>
@@ -32,20 +32,20 @@ function App() {
         </p>
         <p id="author" className='font-light mt-2'>{quote ? '- ' : ""}{author}</p>
       </div>
-      <div className="flex flex-col  sm:flex-row  justify-between gap-10 sm:gap-20 mt-5">
+      <div className={` flex flex-col  sm:flex-row ${quote ? "justify-between" : "justify-center"} gap-10 mt-5`}>
             {quote 
             ? <a
                 id="tweet-quote"
                 href={`https://twitter.com/intent/tweet?text=${quote} - ${author}`}
                 target="_blank"
                 rel="noopener noreferrer"
-                className='text-center'
+                className='text-center '
                 >
                   Tweet this quote
                 </a> 
             :   ""}
       <button id="new-quote" onClick={toggle}>
-           {quote ? "New Quote" : "Genrate a Quete"} 
+          {quote ? "New Quote" : "Genrate a Quete"} 
       </button>
       </div>
     
